3b3b4b7febace96244594f978de226e9353665c7
gakaki <gakaki@gmail.com>
2014-03-16 00:19:43 +0800

    commit all

--- index.php
+++ index.php
@@ -1,11 +1,20 @@
-<?php
-$dir 			= "images";
-$glob_pattern 	= $dir."/*.{jpg,png,gif,PNG,JPG}";
-$data			= array();
-foreach (glob($glob_pattern,GLOB_BRACE) as $filename) {
-    $path =  "<div><img src='{$filename}' /></div>";
-	$data[] = $path;
-	echo $path;
-}
-
-
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>陈建丽的Resume</title>
+  </head>
+  <body>
+    
+	  <?php
+		  $dir 			= "images";
+		  $glob_pattern 	= $dir."/*.{jpg,png,gif,PNG,JPG}";
+		  $data			= array();
+		  foreach (glob($glob_pattern,GLOB_BRACE) as $filename) {
+		      $path =  "<div><img src='{$filename}' /></div>";
+		  	$data[] = $path;
+		  	echo $path;
+		  }
+	  ?>
+	
+  </body>
+</html>
